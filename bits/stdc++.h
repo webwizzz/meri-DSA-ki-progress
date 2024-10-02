11192212012b7d0c023c2d8397f2616f998fabdd
webwizzz <jatin.tomar@somaiya.edu>
2024-10-02 14:48:43 +0530

    include shortcut

---
+++ bits/stdc++.h
@@ -0,0 +1,28 @@
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cstddef>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <numeric>
+#include <limits>
+#include <cstring>
+#include <cstdio>
+#include <functional>
+#include <climits>
+#include <cassert>
+#include <bitset>
+#include <iomanip>
+#include <deque>
+#include <list>
+#include <cmath>
+#include <complex>
+#endif
+
